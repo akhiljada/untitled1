5a42638d58311a96d33fc900d9fd9944cef36e81
Akhil <akhil.jada@zemosolabs.com>
2022-05-18 09:02:19 +0530

    java assignments done

---
+++ src/hello.java
@@ -0,0 +1,8 @@
+public class hello {
+    public static void main(String[] args) {
+        int age=22;
+        String name="Akhil";
+        System.out.println("Welcome to my world");
+        System.out.println(name + " is " + age);
+    }
+}
